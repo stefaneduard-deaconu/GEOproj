aa7efea19e9c4bc3cb629dcd0d0c8b09a5caea47
stefaneduard-deaconu <stefaneduard.deaconu@gmail.com>
2019-07-30 05:05:21 -0700

    fixed little bugs and scopes

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ global variables, used for events
 - points ----> the points of the geometrical figure :D
 '''
 taking_input = True
-points = []
+polygon = []
 
 
 def screen_dimensions():
@@ -22,7 +22,113 @@ def screen_dimensions():
     return user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
 
 
-def main():
+def line(A, B, tad, color):
+    x0, y0, x1, y1 = A[0], A[1], B[0], B[1]
+    tads_position = tad.position()
+    tads_color = tad.color()
+    tad.color(color, color)
+    tad.penup()
+    tad.goto(x0, y0)
+    tad.pendown()
+    tad.goto(x1, y1)
+    tad.penup()
+    tad.goto(tads_position)
+    tad.color(tads_color[0], tads_color[1])
+
+"""
+###############
+Event handlers.
+###############
+"""
+def draw_to_point(x=None, y=None):
+    # if we presses the start button, then ...
+    #
+    print('draw:')
+    tad.goto(x, y)
+    if taking_input:
+        polygon.append((x, y))
+        print(polygon)
+    else:
+        pass
+
+
+def first_point(x=None, y=None):
+    # TODO  if outside, better reset() everything
+    print('first:')
+    tad.penup()
+    tad.goto(x, y)
+    tad.pendown()
+    polygon.clear()
+    polygon.append((x, y))  # first point
+    print(polygon)
+    tad.screen.onclick(draw_to_point, btn=1)
+
+
+def start(event):
+    print(polygon)
+    print('you presses "Start"')
+    # finish the polygon:
+    tad.goto(polygon[0][0], polygon[0][1])
+    # Tad. Go sit
+    tad.penup()
+    tad.shapesize(3, 3)
+    tad.setheading(90)
+    screen_width, screen_height = screen_dimensions()
+    window_width, window_height = 0.618 * screen_width, 0.8 * screen_height
+    tad.goto(window_width * 0.9 // 2, window_height * 0.9 // 2)
+    #
+    from triangles import y_decompose
+    """
+    Function that given the border of a polygon, generates a decomposition
+        of it as y-monotone polygons.
+    """
+    from triangles import triangulate
+    """
+    Function that given a y-monotone polygon,
+        returns one of its triangulations, as a list of triangles.
+    """
+    y_polygons = y_decompose(polygon)
+    triangles = []
+    for poly in y_polygons:
+        triangles += triangulate(poly)
+
+    def draw_triangles():
+        pass
+
+
+def exercise_1(event):
+    print('you presses button "1"')
+
+
+def exercise_2(event):
+    print('you presses button "2"')
+
+
+def exercise_3(event):
+    print('you presses button "3"')
+
+
+def exercise_4(event):
+    print('you presses button "4"')
+
+
+def reset_canvas(event):
+    print('you pressed "Reset"')
+    tad.clear()
+    tad.shapesize(1)
+    screen_width, screen_height = screen_dimensions()
+    window_width, window_height = 0.618 * screen_width, 0.8 * screen_height
+    # using the turtle to first draw the 'coordinate axes' :D
+    line((-window_width * 0.9 // 2, 0), (window_width * 0.9 // 2, 0), tad, 'white')
+    tad.setheading(90)
+    line((0, -window_height * 0.9 // 2), (0, window_height * 0.9 // 2), tad, 'white')
+    tad.color('lime', 'lime')
+    tad.goto(0, 0)
+    # rebind the event handler for capturing the polygon
+
+    tad.screen.onclick(first_point, btn=1)
+
+def set_gui():
     """
     The starting function.
     lays down the ground for the app
@@ -58,12 +164,11 @@ def main():
     tad = turtle.RawTurtle(canvas)
     tad.color('white', 'white')
     tad.speed(10)
-    tad.shapesize(0.5, 0.5)
+    tad.shapesize(1)
 
     # now center the window:
-    root.resizable(True, True)
+    root.resizable(0, 0)
     # the first one must be -7 as to be leftmost on windows 10 :))
-    # let's center the window:
     x_centered = int((screen_width - window_width) // 2)
     y_first_fifth = int((screen_height - window_height) // 5)
     root.geometry('+{}+{}'.format(x_centered, y_first_fifth))  # these correspond
@@ -73,93 +178,27 @@ def main():
 
     # using the turtle to first draw the 'coordinate axes' :D
 
-    def line(A, B, color):
-        x0, y0, x1, y1 = A[0], A[1], B[0], B[1]
-        tads_position = tad.position()
-        tads_color = tad.color()
-        tad.color(color, color)
-        tad.penup()
-        tad.goto(x0, y0)
-        tad.pendown()
-        tad.goto(x1, y1)
-        tad.penup()
-        tad.goto(tads_position)
-        tad.color(tads_color[0], tads_color[1])
-
-    line((-window_width * 0.9 // 2, 0), (window_width * 0.9 // 2, 0), 'white')
+    line((-window_width * 0.9 // 2, 0), (window_width * 0.9 // 2, 0), tad, 'white')
     tad.setheading(90)
-    line((0, -window_height * 0.9 // 2), (0, window_height * 0.9 // 2), 'white')
+    line((0, -window_height * 0.9 // 2), (0, window_height * 0.9 // 2), tad, 'white')
     tad.color('lime', 'lime')
 
-    def draw_to_point(x=None, y=None):
-        # if we presses the start button, then ...
-        #
-        tad.goto(x, y)
-        if taking_input:
-            points.append((x, y))
-        else:
-            pass
-
-    def first_point(x=None, y=None):
-        # TODO  if outside, better reset() everything
-        print('first')
-        tad.penup()
-        tad.goto(x, y)
-        tad.pendown()
-        points.append((x, y))
-        tad.screen.onclick(draw_to_point, btn=1)
-
-    def start(event):
-        print('you presses "Start"')
-        # finish the polygon:
-        tad.goto(points[0][0], points[0][1])
-        tad.penup()
-        tad.shapesize(3, 3)
-        tad.setheading(90)
-        tad.goto(window_width * 0.9 // 2, window_height * 0.9 // 2)
-        from triangles import y_decompose
-        """
-        Function that given the border of a polygon, generates a decomposition
-            of it as y-monotone polygons.
-        """
-        from triangles import triangulate
-        """
-        Function that given a y-monotone polygon,
-            returns one of its triangulations, as a list of triangles.
-        """
-        y_polygons = y_decompose(polygon)
-        triangles = []
-        for poly in y_polygons:
-            triangles += triangulate(poly)
-        def draw_triangles():
-            pass
-
-
-    def reset(event):
-        print('you presses "Reset"')
-
-    def exercise_1(event):
-        print('you presses button "1"')
-
-    def exercise_2(event):
-        print('you presses button "2"')
-
-    def exercise_3(event):
-        print('you presses button "3"')
-
-    def exercise_4(event):
-        print('you presses button "4"')
-
     start_button.bind('<Button-1>', start)
-    reset_button.bind('<Button-1>', reset)
+    reset_button.bind('<Button-1>', reset_canvas)
     button_1.bind('<Button-1>', exercise_1)
     button_2.bind('<Button-1>', exercise_2)
     button_3.bind('<Button-1>', exercise_3)
     button_4.bind('<Button-1>', exercise_4)
 
     tad.screen.onclick(first_point)
-    #
-    # turtle.mainloop()
+
+    return root, tad
+
+
+root, tad = set_gui()
+
+
+def main():
     root.mainloop()
 
 
