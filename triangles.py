02d20a596f704c52172e0c32d4489ef89e5ca0fa
stefaneduard-deaconu <stefaneduard.deaconu@gmail.com>
2019-07-30 04:02:19 -0700

    starting the file that contains the functions for the big requirement :D

---
+++ triangles.py
@@ -0,0 +1,18 @@
+"""
+Here we have two utility functions, for y-decomposition (decomposing a
+  polygon in y-monotone polygons), and the triangulation of a y-monotone
+  polygon.
+Plus the main() function, used throughout the project for testing :D
+"""
+
+
+def y_decompose(polygon):
+    pass
+
+
+def triangulate(y_polygon):
+    pass
+
+
+if __name__ == '__main__':
+    main()
